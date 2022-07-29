82d205a50d7ee94f5f1a00e1f162dcb0c1046ba2
Mauricio Rojas <mrojasb2000@gmail.com>
2022-07-29 17:11:24 -0400

    chore: add runtime safety build mode

--- test_01.zig
+++ test_01.zig
@@ -2,5 +2,8 @@ test "integer overflow at compile time" {
     // error: operation caused overflow
     //const x: u8 = 255;
     var x: u8 = 255;
+    // Disable safety build mode
+    //@setRuntimeSafety(false);
+    //x += 1; // Exceder el tamaño del tipo de datos u8
     _ = x + 1;
 }
